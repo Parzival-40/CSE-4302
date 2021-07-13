bd8f71d7062333408c9c05a568adf346d933ca32
Md Abdullahil Kafi <abdullahilkafi@iut-dhaka.edu>
2021-07-13 14:16:02 +0600

    Lab 6

---
+++ Lab 4/224 task 1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+class BankAccount {
+private:
+	long number;
+	string holder_name;
+	string type;
+	double current_balance;
+	double minimum_balance;
+
+public:
+	BankAccount(long number, string holder_name, string type, double current_balance, double minimum_balance)
+	  : number(number), holder_name(holder_name), type(type), current_balance(current_balance), minimum_balance(minimum_balance) {}
+	~BankAccount() {
+		cout << "Account of " << holder_name
+			 << " with account number " << number
+			 << " is destroyed with a balance BDT " << current_balance << endl;
+	}
+	void info() {
+		cout << "Account Holder: " << holder_name << endl
+			 << "Account number: " << number << endl
+			 << "Account Type: " << type << endl
+			 << "Current balance: " << current_balance << endl
+			 << "Minimum balance: " << minimum_balance << endl;
+	}
+	void showBalance() {
+		cout << "Current balance: " << current_balance << endl;
+	}
+	void diposite(double amount) {
+		if (amount <= 0) return;
+		current_balance += amount;
+	}
+	void withdraw(double amount) {
+		if (current_balance - amount < minimum_balance) return;
+		current_balance -= amount;
+	}
+	void giveInterest(double interest = 3) {
+		double net_interest;
+		net_interest = current_balance * interest / 100;
+		current_balance += (net_interest - net_interest * .1);
+	}
+};
+
+int main() {
+	BankAccount a(224, "Kafi", "Current", 1234, 12);
+	a.info();
+	a.showBalance();
+	a.diposite(12345);
+	a.withdraw(12312412);
+	a.withdraw(1234);
+	a.showBalance();
+	a.giveInterest();
+	a.showBalance();
+	a.giveInterest(10);
+	a.showBalance();
+}
---
+++ Lab 4/224 task 2.cpp
@@ -0,0 +1,60 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+private:
+	double width, length;
+
+public:
+	Rectangle(double width = 1, double length = 1)
+	  : width(width), length(length) {}
+	double Perimeter() {
+		return 2 * (width + length);
+	}
+	double area() {
+		return width * length;
+	}
+	double diagonal() {
+		return sqrt(width * width + length * length);
+	}
+	double angle_between_length_and_diagonal() {
+		return atan(length / width);
+	}
+	void set_width(double width) {
+		if (width < 1 || width > 20) return;
+		this->width = width;
+	}
+	double get_width() {
+		return width;
+	}
+	void set_length(double length) {
+		if (length < 1 || length > 20) return;
+		this->length = length;
+	}
+	double get_length() {
+		return length;
+	}
+};
+
+int main() {
+	Rectangle a(20, 10);
+
+	cout << "Length: " << a.get_length() << endl
+		 << "Width: " << a.get_width() << endl
+		 << "Area: " << a.area() << endl
+		 << "Perimeter: " << a.Perimeter() << endl
+		 << "Diagonal: " << a.diagonal() << endl
+		 << "Angle between length and diagonal: " << a.angle_between_length_and_diagonal() << endl;
+
+	a.set_width(10);
+	a.set_length(20);
+
+	cout << "Length: " << a.get_length() << endl
+		 << "Width: " << a.get_width() << endl
+		 << "Area: " << a.area() << endl
+		 << "Perimeter: " << a.Perimeter() << endl
+		 << "Diagonal: " << a.diagonal() << endl
+		 << "Angle between length and diagonal: " << a.angle_between_length_and_diagonal() << endl;
+}
