48be6ca4c5c91dc3371416b5d942a796b41fd843
Md Abdullahil Kafi <abdullahilkafi@iut-dhaka.edu>
2021-07-15 10:30:01 +0600

    Lab-06 corrected

--- Lab 6/224 task 3.cpp
+++ Lab 6/224 task 3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+
 class Float {
 private:
 	float data;
@@ -9,31 +10,30 @@ public:
 	Float(float data = 0)
 	  : data(data) {}
 
-	Float operator=(const float& a) {
-		return Float(data = a);
-	}
 	friend Float operator+(const Float& a, const Float& b);
 	friend Float operator-(const Float& a, const Float& b);
 	friend Float operator*(const Float& a, const Float& b);
 	friend Float operator/(const Float& a, const Float& b);
 
-	friend ostream& operator<<(ostream& out, Float a) {
-		out << a.data;
-		return out;
-	}
+	friend ostream& operator<<(ostream& out, Float a);
 };
 
+ostream& operator<<(ostream& out, Float a) {
+	out << a.data;
+	return out;
+}
+
 Float operator+(const Float& a, const Float& b) {
-	return Float(a + b);
+	return Float(a.data + b.data);
 }
 Float operator-(const Float& a, const Float& b) {
-	return Float(a - b);
+	return Float(a.data - b.data);
 }
 Float operator*(const Float& a, const Float& b) {
-	return Float(a * b);
+	return Float(a.data * b.data);
 }
 Float operator/(const Float& a, const Float& b) {
-	return Float(a / b);
+	return Float(a.data / b.data);
 }
 
 int main() {
@@ -41,11 +41,11 @@ int main() {
 	f1 = 2.5;
 	f2 = 3.1;
 	cout << "f1+f2: "
-		 << f1 + f2 << endl
+		 << (f1 + f2) << endl
 		 << "f1-f2: "
-		 << f1 - f2 << endl
+		 << (f1 - f2) << endl
 		 << "f1*f2: "
-		 << f1 * f2 << endl
+		 << (f1 * f2) << endl
 		 << "f1/f2: "
-		 << f1 / f2 << endl;
+		 << (f1 / f2) << endl;
 }
